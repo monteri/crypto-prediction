0a7e5d96e99ff0eb7af7a443a3d72ac9e79d04d9
KostiaPlugarev <kostyaplugarev@gmail.com>
2025-07-24 18:18:43 +0300

    add react route

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<h2>hui</h2>} />
+    </Routes>
+  </BrowserRouter>
+);
